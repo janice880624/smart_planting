fe8132a2664e2eeecfaa3729bbb7bf6bb7cb4222
janice880624 <47550853+janice880624@users.noreply.github.com>
2022-03-25 22:40:50 +0800

    js

---
+++ app.js
@@ -0,0 +1,16 @@
+var config = {
+    apiKey: "AIzaSyChSx34wzEPlSUED3gjM34m07qUlV-QHmo",
+    authDomain: "smart-planting-ddad1.firebaseapp.com",
+    databaseURL: "https://smart-planting-ddad1-default-rtdb.firebaseio.com",
+    projectId: "smart-planting-ddad1",
+    storageBucket: "smart-planting-ddad1.appspot.com",
+    messagingSenderId: "934197283911",
+    appId: "1:934197283911:web:144de621766e26ae56b562"
+};
+
+firebase.initializeApp(config);
+
+// db.ref("/dht").once('value', function (snapshot) {
+//     var data = snapshot.val();
+//     console.log(data);
+// });
