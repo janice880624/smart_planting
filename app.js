7baac24676eb17ccacd671d522742ae46928c660
janice880624 <47550853+janice880624@users.noreply.github.com>
2022-04-09 21:00:56 +0800

    0409

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-var url = 'https://docs.google.com/spreadsheets/d/1i8nsUwJ9aJJ9tbJifoww9mlNA1bM_VN2LPeWA-kmyJk/edit?usp=sharing';
-var name = '工作表1'
-var SpreadSheet = SpreadsheetApp.openByUrl(url);
-var SheetName = SpreadSheet.getSheetByName(name);
-var lastColumn = SheetName.getLastColumn();
-var lastRow = SheetName.getLastRow();
-
-console.log(SheetName.getSheetValues(1,1,lastRow,lastColumn))
+$.get('https://script.google.com/macros/s/AKfycbze28MRBYGAaHke9pTxOhBUEcLje68T_gcMxPTUOP40toaFVhF0/exec',function(e){
+  console.log(e);
+});
