8faded68a5bf041a8e0dcf35ccffda376b2479a2
janice880624 <47550853+janice880624@users.noreply.github.com>
2022-04-09 18:25:40 +0800

    0409

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
-var g3;
-var photocell;
-var myData;
-var dht;
-var water;
-var hum;
-var ph;
-var soil;
-var pm10;
-var pm25;
-var tem;
+var url = 'https://docs.google.com/spreadsheets/d/1i8nsUwJ9aJJ9tbJifoww9mlNA1bM_VN2LPeWA-kmyJk/edit?usp=sharing';
+var name = '工作表1'
+var SpreadSheet = SpreadsheetApp.openByUrl(url);
+var SheetName = SpreadSheet.getSheetByName(name);
+var lastColumn = SheetName.getLastColumn();
+var lastRow = SheetName.getLastRow();
 
-
-myData= {};
-myData.sheetUrl = 'https://docs.google.com/spreadsheets/d/1i8nsUwJ9aJJ9tbJifoww9mlNA1bM_VN2LPeWA-kmyJk/edit?usp=sharing';
-myData.sheetName = '工作表1';
-readSheetData({
-  row : 1,
-  col : 4,
-  sheetUrl : myData.sheetUrl,
-  sheetName : myData.sheetName
-}, function(googleSheetReadData){
-  document.getElementById('demo-area-01-show').innerHTML = googleSheetReadData;
-});
+console.log(SheetName.getSheetValues(1,1,lastRow,lastColumn))
